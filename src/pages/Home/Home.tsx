097cc2240ce74b6978a050f704c340692f7166ae
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2025-07-11 13:20:02 +0300

    home-page. Добавлена карточка книги

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,9 @@
+import BookCard from "@/common/components/BookCard/BookCard";
 import SearchInput from "@/common/components/SearchInput/SearchInput";
 import SortDropdown from "@/common/components/SortDropdown/SortDropdown";
 import styles from "@/pages/Home/Home.module.css";
+
+const book = { coverUrl: "", title: "1984 Ешенавп вапавпав", author: "Jeorge Oruell", description: "gdfgj fhkjfdg fdghdfkg dfgjdf hgkfdgh d hfdjkgh hdfdjgh dkfjgh dfkjg hdfjghfd khgjkd gdg hdkfdgh khfdkgh dgkdfg kdhfdk dkdfgdhkghdf hdfghdfgkd dhfgkfdg dkfhg d" };
 
 export default function Home() {
     return (
@@ -8,6 +11,9 @@ export default function Home() {
             <div className={styles.home__filters}>
                 <SearchInput />
                 <SortDropdown />
+            </div>
+            <div className={styles.home__books}>
+                {[...Array(10).keys()].map((key) => <BookCard key={key} {...book} />)}
             </div>
         </div>
     )
